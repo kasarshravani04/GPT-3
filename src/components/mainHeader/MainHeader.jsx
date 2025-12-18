a4902ce1301acccac45dd453b593a4756d8dc1c6
kasarshravani04 <shravanikasar05@gmail.com>
2025-12-18 12:42:26 +0530

    set the burger menu

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './MainHeader.css';
 import  logo  from '../../assets/Logo/GPT-3.svg';
 import Button from '../../UI/Button/Button';
@@ -6,19 +6,14 @@ import { useState } from 'react';
 
 const MainHeader = () => {
     const [open,setOpen] = useState(false);
-    let style = {};
-    if(open){
-        style.top = "0%";   
-    }else{
-        style.top = "-100%";
-    }
-      
-  return (
+       console.log("component rendered", open);
+    return (
     <header className="main_header layout">
         <div className="main_header__logo">
             <img src={logo} alt='Gpt-3' loading='eager' width={150} height={100} />
         </div>
-        <nav style={style}>
+        <nav 
+        style={open ?{top : "0%"} : {top : "-2000%"}}>
             <ul>
                 <li>
                     <a href="#">Home</a>
@@ -42,12 +37,10 @@ const MainHeader = () => {
             <Button active = {true}>Sign Up</Button>
         </div>
         <label className="main_header__hamburger">
-        <input type="checkbox" />
-        <svg
-          viewBox="0 0 32 32"
-          onClick={() => {
-            setOpen(true);
-          }}>
+        <input type="checkbox"
+          checked = {open}
+          onClick={() => setOpen((prev) => !prev)} />
+        <svg viewBox='0 0 32 32'>
           <path
             className="line line-top-bottom"
             d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
@@ -55,6 +48,7 @@ const MainHeader = () => {
         </svg>
         </label>
     </header>
+
   );
 };
 
