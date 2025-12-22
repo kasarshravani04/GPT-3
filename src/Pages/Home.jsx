df974bbf9400141b46c85a819d02777b4edd341e
kasarshravani04 <shravanikasar05@gmail.com>
2025-12-22 13:14:17 +0530

    added banner section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
 import About from "../components/About/About";
+import Client from "../components/Client/Client";
 
 
 const Home = () => {
   return (
     <>
        <Banner />
+       <Client/>
        <About/>
     </>
   );
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+
 import "./Banner.css";
 import banner_img from "../../assets/Banner/Banner.png";
+import c1 from "../../assets/Banner/c1.png";
+import c2 from "../../assets/Banner/c2.png";
+import c3 from "../../assets/Banner/c3.png";
+import c4 from "../../assets/Banner/c4.png";
+import c5 from "../../assets/Banner/c5.png";
+import c6 from "../../assets/Banner/c6.png";
 import GradientText from "../../UI/GradientText/GradientText";
 import Button from "../../UI/Button/Button";
 
@@ -24,6 +30,23 @@ const Banner = () => {
             <input type="email"  placeholder="your email address"  id="Email" name="Email" autoComplete="false"/>
             <Button active={true}>Get Started</Button>
           </div>
+          
+         <div className="banner__footer_section">
+            <div className="img_container">  
+            <img src={c1} alt="person 1" width={50} height={50} />
+            <img src={c2} alt="person 2" width={50} height={50} />
+            <img src={c3} alt="person 3" width={50} height={50} />
+            <img src={c4} alt="person 4" width={50} height={50} />
+            <img src={c5} alt="person 5" width={50} height={50} />
+            <img src={c6} alt="person 6" width={50} height={50} />
+            <span className="last_img">1.6K+</span>
+            </div>
+          <div>
+             <p>
+               1,600 people requested access a visit in last 24 hours
+             </p>
+          </div>
+         </div>
       </div>
       <div className='banner_section__right'>
         <img src={banner_img} alt='AI'  loading='eager' width={300} height={400}/>
