c5f3c47ef9da8b63c4cf32ce5e95eaf294440dfd
kasarshravani04 <shravanikasar5@gmail.com>
2025-12-23 14:53:33 +0530

    created feature section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner/Banner";
 import Client from "../components/Client/Client";
 import About from "../components/About/About";
 import Services from "../components/Services/Services";
+import Feature from "../components/Feature/Feature";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
        <Client/>
        <About/>
        <Services/>
+       <Feature/>
     </>
   );
 };
